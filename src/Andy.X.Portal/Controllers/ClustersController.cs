7c435f0f8d2ceb1e752fbe7c372fda276412f0c2
Enes Hoxha <eneshoxha@outlook.com>
2022-07-19 00:47:14 +0200

    v3/feature/6 Import new UI for Andy X Portal

---
+++ src/Andy.X.Portal/Controllers/ClustersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Andy.X.Portal.Controllers
+{
+    public class ClustersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
